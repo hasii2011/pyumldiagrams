878ba0fb2685c7b52fce1f9f2cccdc0093265085
Humberto Sanchez II <humberto.a.sanchez.ii@gmail.com>
2020-11-17 09:44:51 -0600

    <Release>[Version #]: Update for Pypi

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="pyumldiagrams",
-    version="2.2",
+    version="2.21",
     description="Draw UML diagrams in various format",
     long_description=README,
     long_description_content_type="text/markdown",
