31ccc0b229d0c4d815563fccd7c9c2696816e55a
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2021-03-20 14:56:22 -0500

    <Release>[Prep]: Bump version prep for release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="pyumldiagrams",
-    version="2.22",
+    version="2.3.0",
     description="Draw UML diagrams in various format",
     long_description=README,
     long_description_content_type="text/markdown",
