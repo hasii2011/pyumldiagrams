55de0dff2575370f4bd8dff05357e65fd50079fe
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2023-07-12 19:44:51 -0500

    <>[]: <Start using Sphinx>
    []
    
    [https://github.com/hasii2011/pyumldiagrams/issues/51]

---
+++ docs/source/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'PyUmlDiagrams'
+copyright = '2023, Humberto A. Sanchez II'
+author = 'Humberto A. Sanchez II'
+release = '2.50.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# extensions = ["myst_parser"]
+# extensions = []
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'classic'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,22 @@
+.. PyUmlDiagram documentation master file, created by
+   sphinx-quickstart on Wed Jul 12 15:05:39 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to PyUmlDiagram's documentation!
+=========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Table of Contents:
+   api
+   usage
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
